1e2d08feccd5f77ef2daecad2a7cff9f85feeed1
arinkocom <aricom0404@gmail.com>
2019-10-18 21:21:55 +0900

    change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,7 +39,7 @@ $(function(){
       
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__message').val('');
+      $('.new_message')[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
     })
     . fail(function(){
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
     
     @message = @group.messages.new(message_params)
     if @message.save
-      respond_to do |format|
+        respond_to do |format|
         format.json
       end
     else
