de7d6f398f36e4097d8bcbc555fd8987a98bf15d
arinkocom <aricom0404@gmail.com>
2019-10-20 12:17:57 +0900

    change

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -18,9 +18,10 @@ $(function(){
                 <p class="chat-group-user__name">ユーザーが見つかりません</p>
               </div>`
   search_list.append(html);
+    return html;
   }
-  $(function(){
-  $("#user-search-field_text").on('keyup', function(){
+  
+  $("#user-search-field").on('keyup', function(){
     var input = $(this).val();
     
   
@@ -45,8 +46,29 @@ $(function(){
   })
     .fail(function() {
       alert("ユーザー検索に失敗しました");
+      return false;
     })
-  });
-  })
+    
+      
+    });
+        
+    $(document).on('click', '.user-search-add', function() {
+            var name = $(this).data("user-name");
+            var user_id = $(this).data("user-id");
+            $(this).parent().remove();
+            console.log(name)
+          });
+      function appendMembers(name, user_id);
+      var html = `
+      <div class='chat-group-user'>
+          <input name='group[user_ids][]' type='hidden' value='${user_ids}'>  
+        <p class='chat-group-user__name'>${user_name}</p>
+        <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+      </div>
+      `
+  $().append(html)
+}
 })
+      
+
 
